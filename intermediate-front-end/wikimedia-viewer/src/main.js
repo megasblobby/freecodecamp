8fad3514122e239865afb851e1b0602aa8387038
megasblobby <so4@protonmail.com>
2017-11-07 14:57:34 +0100

    Added preliminary results visualization to wikimedia-viewer

---
+++ intermediate-front-end/wikimedia-viewer/src/main.js
@@ -0,0 +1,54 @@
+/*https://en.wikipedia.org/w/api.php?action=opensearch&format=xmlfm&search=api&namespace=0&limit=10*/
+const REQUEST_DONE = 4;
+const RESPONSE_READY = 200;
+
+window.onload = function() {
+  makeRequest().then(response => displayResponse(response));
+}
+
+function makeRequest() {
+  return new Promise((resolve, reject) => {
+    let request = new XMLHttpRequest();
+    request.responeType = "document";
+    request.overrideMimeType('text/xml');
+
+    let url =
+     `https://en.wikipedia.org/w/api.php?action=opensearch&origin=*&format=xml&search=cobra&namespace=0&limit=10`;
+    request.open('GET', url, true);
+    //request.setRequestHeader("Origin", "https://www.yourpage.com");
+    request.onload = () => {
+          if (request.readyState === REQUEST_DONE && request.status === RESPONSE_READY) {
+              resolve(request.responseXML);
+          } else {
+              reject(request.statusText);
+          }
+      };
+    request.onerror = () => reject(request.statusText);
+    request.send();
+  });
+}
+
+function displayResponse(response) {
+  console.log(response);
+  let items = response.getElementsByTagName('Item');
+  let answers = document.getElementById('answers');
+  for (let item of items) {
+    let answer = document.createElement('div');
+
+    let title = document.createElement('h1');
+    title.innerHTML = item.getElementsByTagName('Text')[0].innerHTML;
+    
+    let description = document.createElement('p');
+    description.innerHTML = item.getElementsByTagName('Description')[0].innerHTML;
+
+    let url = item.getElementsByTagName('Url')[0].innerHTML;
+    let link = document.createElement('a');
+    link.href = url;
+    link.appendChild(title);
+    link.appendChild(description);
+
+    answers.appendChild(link);
+  }
+
+  console.log(items);
+}
