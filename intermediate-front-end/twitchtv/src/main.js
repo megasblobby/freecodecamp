0fab37b56847927f95766608d8f39f76f11d205d
megasblobby <so4@protonmail.com>
2018-01-03 09:53:42 +0100

    Started to implement the animated menu to select streams categories

--- intermediate-front-end/twitchtv/src/main.js
+++ intermediate-front-end/twitchtv/src/main.js
@@ -4,22 +4,33 @@ const REQUEST_DONE = 4;
 const RESPONSE_READY = 200;
 const CLIENT_ID = 'q83ev6ap7bo9pea30lg0gdkhem0ekt';
 const BASE_URL = 'https://api.twitch.tv/helix/';
-const USERS = 'users?'
-const STREAMS = 'streams?'
-const OFFLINE = 'offline'
+const USERS = 'users?';
+const STREAMS = 'streams?';
+const OFFLINE = 'offline';
+const NO_ID = '';
 
 let usersLogin = ["ESL_SC2", "OgamingSC2", "cretetion", "freecodecamp",
                     "storbeck", "habathcx", "RobotCaleb", "noobs2ninjas"];
 
 let users = new Map();
 let streams = new Map();
+let flags = {onlineAreVisible : true, offlineAreVisible : true};
+let categories = {};
 
 window.onload = () => {
+  let elements = document.getElementsByClassName('category');
+  for (var index = 0; index < elements.length; index++) {
+    elements[index].onclick = setFlags;
+    categories[`${elements[index].id}`] = false;
+  }
+
   getUsersInfo(usersLogin).then(response => {
     let usersData = JSON.parse(response);
     let data = usersData.data;
     data.forEach((userData) => {
       users.set(userData.id, userData);
+      // Remove id property because is already used as key
+      delete users.get(userData.id)['id'];
     });
 
     let keys = Array.from(users.keys());
@@ -31,12 +42,52 @@ window.onload = () => {
       let data = streamsData.data;
       data.forEach((streamData) => {
         streams.set(streamData.user_id, streamData);
+        // Remove user_id property because is already used as key
+        delete streams.get(streamData.user_id)['user_id'];
       });
-
-      console.log(users);
-      console.log(streams);
+      displayResults(users, streams, flags);
     });
   });
+}
+
+function setFlags(event) {
+  let target = document.getElementById(event.target.id);
+  if (event.target.id === NO_ID) {
+    target = event.target.parentElement;
+  }
+
+  if (event.target.id === 'all-category') {
+    target.onlineAreVisible = true;
+    target.offlineAreVisible = true;
+  }
+  if (event.target.id === 'online-category') {
+    target.onlineAreVisible = true;
+    target.offlineAreVisible = false;
+  }
+  if (event.target.id === 'offline-category') {
+    target.onlineAreVisible = false;
+    target.offlineAreVisible = true;
+  }
+
+  displayResults(users, streams, flags);
+
+  if (categories[target.id] === false) {
+    target.animate([
+      { width: '18px', left: '54px'},
+      { width: '70px', left: '0px'}],
+      { duration: 200, fill: 'both' }).onfinish = () => {
+          categories[target.id] = !categories[target.id];
+      };
+  }
+  else {
+    target.animate([
+      { width: '70px', left: '0px'},
+      { width: '18px', left: '54px'}],
+      { duration: 200, fill: 'both' }).onfinish = () => {
+          categories[target.id] != categories[target.id];
+      };
+  }
+
 }
 
 function getUsersInfo(usersLogin) {
@@ -48,7 +99,6 @@ function getUsersInfo(usersLogin) {
 
     return makeRequest(url);
 }
-
 
 function getStreamsInfo(usersID) {
   let url =`${BASE_URL}${STREAMS}user_id=${usersID.shift()}`;
@@ -76,4 +126,88 @@ function makeRequest(url) {
     request.onerror = () => reject(request.statusText);
     request.send();
   });
+}
+
+function displayResults(users, streams, flags) {
+  clear();
+  let online = document.getElementById('online');
+  let offline = document.getElementById('offline');
+  let usersIDs = Array.from(streams.keys());
+
+  for (let id of usersIDs) {
+    let target = null;
+    let user = users.get(id);
+    let stream = streams.get(id);
+
+    let streamContainer = getStreamContainer();
+    let link = getLink(user.login, user.display_name);
+    let profileImage = getProfileImage(user.profile_image_url);
+    let title = null;
+
+    if (stream === OFFLINE && flags.offlineAreVisible) {
+      title = getTitle(OFFLINE);
+      target = offline;
+      streamContainer.appendChild(profileImage);
+      streamContainer.appendChild(link);
+      streamContainer.appendChild(title);
+
+          target.appendChild(streamContainer);
+    } else if(stream !== OFFLINE && flags.onlineAreVisible){
+      title = getTitle(stream.title);
+      target = online;
+      streamContainer.appendChild(profileImage);
+      streamContainer.appendChild(link);
+      streamContainer.appendChild(title);
+
+          target.appendChild(streamContainer);
+    }
+
+  }
+}
+
+function clear() {
+  let online = document.getElementById('online');
+  let offline = document.getElementById('offline');
+
+  let children = online.childNodes;
+  while(children.length > 0) {
+    online.removeChild(children[0]);
+  }
+  children = offline.childNodes;
+  while(children.length > 0) {
+    offline.removeChild(children[0]);
+  }
+}
+
+function getStreamContainer() {
+  let streamContainer = document.createElement('div');
+  streamContainer.setAttribute('class', 'container');
+
+  return streamContainer;
+}
+
+function getLink(login, displayName) {
+  let link = document.createElement('a');
+  link.innerHTML = displayName;
+  link.href = `https://www.twitch.tv/${login}`;
+
+  return link;
+}
+
+function getProfileImage(url, displayName) {
+  let profileImage = document.createElement('img');
+  profileImage.src = url;
+  profileImage.alt = `${displayName}'s profile image`;
+
+  profileImage.setAttribute('class', 'profile-image');
+
+  return profileImage;
+}
+
+function getTitle(title) {
+  let streamTitle = document.createElement('span');
+  streamTitle.innerHTML = title;
+  streamTitle.setAttribute('class', 'title');
+
+  return streamTitle;
 }
